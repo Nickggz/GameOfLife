9f850ce04187af64038f6e6ac42e9e4e81e5a38b
Nickggz <110862793+Nickggz@users.noreply.github.com>
2023-09-07 15:11:06 -0300

    Create FileReader.cs

---
+++ src/Library/FileReader.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Text;
+
+namespace Library;
+
+
+public class FileReader
+{
+    public Archivo(string link)
+    {
+        this.url = link;
+        this.content = File.ReadAllText(url);
+        this.contentLines = content.Split('\n');
+        this.board = new bool[contentLines.Length, contentLines[0].Length];
+        for (int y = 0; y < contentLines.Length; y++)
+        {
+            for (int x = 0; x < contentLines[y].Length; x++)
+            {
+                if (contentLines[y][x] == '1')
+                {
+                    board[x, y] = true;
+                }
+            }
+        }
+    }
+    public string url;
+    public string content;
+    public string[] contentLines;
+    public bool[,] board;
